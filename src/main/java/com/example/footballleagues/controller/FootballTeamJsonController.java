8c7676205e4b0cc085f52f1edc18515e341c8bb3
maksimstefanovich <maxim.stefanovich85@gmail.com>
2021-10-05 16:36:44 +0200

    fix bag and add deletemaping

--- src/main/java/com/example/footballleagues/controller/FootballTeamJsonController.java
+++ src/main/java/com/example/footballleagues/controller/FootballTeamJsonController.java
@@ -5,6 +5,7 @@ import com.example.footballleagues.repository.FootballTeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -52,5 +53,14 @@ public class FootballTeamJsonController {
         Optional<FootballTeam> optionalFootballTeam = teamRepository.findById(id);
 
         return optionalFootballTeam.get();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTeamById(@PathVariable("id") long id) {
+        Optional<FootballTeam> optionalFootballTeam = teamRepository.findById(id);
+        if(optionalFootballTeam.isPresent()){
+        FootballTeam footballTeam = optionalFootballTeam.get();
+        teamRepository.delete(footballTeam);
+        }
     }
 }
